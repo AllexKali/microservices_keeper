efce87510b9599f31fe3f5f3fef68776243446f5
Tasya <166902283+Tasyyyaaaa@users.noreply.github.com>
2024-06-06 13:30:53 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,9 @@
+import requests
+
+data = {
+    'table_number': 3,
+    'items': 'Item 5, Item 6',
+}
+
+response = requests.post('http://localhost:5000/orders', json=data)
+print(response.status_code)  # output 201
